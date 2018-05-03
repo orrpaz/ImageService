24f776c381f9ddabd252b12d66b2d9c644be00bd
Or <Or@DESKTOP-FL9V3LU>
2018-05-03 17:13:51 +0300

    get config command

---
+++ ImageService/ImageService/Commands/GetConfigCommand.cs
@@ -0,0 +1,19 @@
+﻿using ImageService.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService.Commands
+{
+    class GetConfigCommand : ICommand
+    {
+        public string Execute(string[] args, out bool result)
+        {
+            ConfigInfomation info = ConfigInfomation.CreateConfigInfomation();
+            result = true;
+            return info.ToJson();
+        }
+    } 
+}
--- ImageServiceGUI/ViewModel/VMSetting.cs
+++ ImageServiceGUI/ViewModel/VMSetting.cs
@@ -1,13 +1,65 @@
-﻿using System;
+﻿using ImageServiceGUI.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ImageServiceGUI.ViewModel
 {
-    class VMSetting
+    class VMSetting : INotifyPropertyChanged
     {
+        private SettingModel settingModel;
+        private ObservableCollection<string> lbHandlers = new ObservableCollection<string>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+
+
+        }
+        public VMSetting()
+        {
+            settingModel = new SettingModel();
+            settingModel.PropertyChanged +=
+            delegate (Object sender, PropertyChangedEventArgs e) {
+            NotifyPropertyChanged("VM_"+e.PropertyName);
+      };
+
+            // continue..
+        }
+
+        public ObservableCollection<string> LbHandlers
+        {
+            get { return lbHandlers; }
+        }
+        public string VM_OutputDirectory
+        {
+            get { return settingModel.OutputDirectory; }
+        }
+
+        public string VM_SourceName
+        {
+            get { return settingModel.SourceName; }
+        }
+
+        public string VM_LogName
+        {
+            get { return settingModel.LogName; }
+        }
+
+        public int VM_ThumbnailSize
+        {
+            get { return settingModel.ThumbnailSize; }
+        }
+
+      
+
 
     }
 }
