26f8c910b13c15821b959b1799adc40fce8d5c48
Amir <bagaona>
2018-05-04 04:10:06 +0300

    להמשיך הנדל קליינט, וליצור באופן דומה גם בשני. להפעיל את זה דרך הראשי

---
+++ ImageService/ImageService/Server/ClientHandler.cs
@@ -0,0 +1,41 @@
+﻿using ImageService.Controller;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService.ImageService.Server
+{
+    class ClientHandler : IClientHandler
+    {
+        private IImageController m_controller;
+
+        public ClientHandler(IImageController controler)
+        {
+            m_controller = controler;
+        }
+
+        public void HandleClient(TcpClient client)
+        {
+            new Task(() =>
+            {
+                using (NetworkStream stream = client.GetStream())
+                using (StreamReader reader = new StreamReader(stream))
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    string commandLine = reader.ReadLine();
+                    Console.WriteLine("Got command: {0}", commandLine);
+
+                    //לשנות בהתאם לפורמט שכתבנו לקבלת פקודה
+                    string result = m_controller.ExecuteCommand((int) commandLine, )
+//                    string result = ExecuteCommand(commandLine, client);
+                    writer.Write(result);
+                }
+                client.Close();
+            }).Start();
+        }
+    }
+}
