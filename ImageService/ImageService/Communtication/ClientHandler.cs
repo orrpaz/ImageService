6b3be02ddaa5c6df4c6650be2af4d59193b17ae7
Amir <bagaona>
2018-05-06 04:09:24 +0300

    TCP עם העברת פרמטרים כולל תיקון לקונפיג-אינפו

--- ImageService/ImageService/Communtication/ClientHandler.cs
+++ ImageService/ImageService/Communtication/ClientHandler.cs
@@ -34,13 +34,13 @@ namespace ImageService.Server
                 m_activeClients.Add(client);
              //   while (clientConnect)
                 {
-
-
                     using (NetworkStream stream = client.GetStream())
                     using (BinaryReader reader = new BinaryReader(stream))
                     using (BinaryWriter writer = new BinaryWriter(stream))
                     {
                         m_logging.Log("waiting to client..", MessageTypeEnum.INFO);
+                        ConfigInfomation cInfo = ConfigInfomation.Create();
+                        string st = cInfo.ToJson();
                         string commandLine = reader.ReadString();
                         Console.WriteLine("Got command: {0}", commandLine);
                         m_logging.Log("ok " + commandLine, MessageTypeEnum.INFO);
@@ -48,7 +48,7 @@ namespace ImageService.Server
                         //לשנות בהתאם לפורמט שכתבנו לקבלת פקודה
                         //string result = m_controller.ExecuteCommand((int)commandLine, null);
                         //                    string result = ExecuteCommand(commandLine, cl ient);
-                        writer.Write("i got" + commandLine);
+                        writer.Write("i got " + st);
                     }
                 }
                 client.Close();
--- ImageService/ImageService/ConfigInfomation.cs
+++ ImageService/ImageService/ConfigInfomation.cs
@@ -12,10 +12,10 @@ namespace ImageService
 {
     class ConfigInfomation
     {
-        private static ConfigInfomation configInfomation;
 
-       // public JObject jsonObject { get; }
-       // public string json { get; }
+        private static ConfigInfomation configInfomation;
+        // public JObject jsonObject { get; }
+        // public string json { get; }
         public string[] handlerPaths { get; }
         public string outputDir { get; }
         public string eventSourceName { get; }
@@ -24,23 +24,19 @@ namespace ImageService
 
         private ConfigInfomation()
         {
-            string[] handlerPaths = ConfigurationManager.AppSettings["Handler"].Split(';');
-            string outputDir = ConfigurationManager.AppSettings["OutputDir"];
-            int thumbnailSize = Int32.Parse(ConfigurationManager.AppSettings["ThumbnailSize"]);
-            string logName = ConfigurationManager.AppSettings["LogName"];
-            string eventSourceName = ConfigurationManager.AppSettings["SourceName"];
-
-        
+            handlerPaths = ConfigurationManager.AppSettings["Handler"].Split(';');
+            outputDir = ConfigurationManager.AppSettings["OutputDir"];
+            thumbnailSize = Int32.Parse(ConfigurationManager.AppSettings["ThumbnailSize"]);
+            logName = ConfigurationManager.AppSettings["LogName"];
+            eventSourceName = ConfigurationManager.AppSettings["SourceName"];
             // dynamic jsonObject = new JObject();
             // jsonObject.handlerPaths = JsonConvert.SerializeObject(handlerPaths);
             //  jsonObject.outputDir = outputDir;
             //   jsonObject.thumbnailSize = thumbnailSize;
             //   jsonObject.logName = logName;
             //   jsonObject.eventSourceName = eventSourceName;
-
-
-
         }
+
         public string ToJson()
         {
             //JObject jsonObject = new JObject {
@@ -61,7 +57,7 @@ namespace ImageService
             return jsonObject.ToString();
         }
 
-        public static ConfigInfomation CreateConfigInfomation()
+        public static ConfigInfomation Create()
         {
             if (configInfomation == null)
             {
